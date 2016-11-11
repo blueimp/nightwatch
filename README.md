0ad246e957336d8a4de7fd39572d035ff4678976
Sebastian Tschan <mail@blueimp.net>
2016-11-11 04:28:51 +0100

    Add screen recording option.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Nightwatch.js Dockerfile
+Dockerfile for [Nightwatch.js](http://nightwatchjs.org/).
 
 ## Usage
 Run the nightwatch tests:
@@ -10,6 +11,18 @@ Connect to the chromedriver via VNC:
 ```sh
 VNC_HOST="$(echo "${DOCKER_HOST:-localhost}" | sed 's#.*/##;s#:.*##')"
 open vnc://user:secret@"$VNC_HOST":5900
+```
+
+Record a VNC session of the tests as FLV file:
+```sh
+docker-compose up -d record
+docker-compose run --rm nightwatch
+docker-compose stop record
+```
+
+Transcode the recorded FLV files to QuickTime compatible MP4 videos:
+```sh
+docker-compose run --rm transcode
 ```
 
 Stop and remove the docker-compose container set:
--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,31 @@
 module.exports = {
-  'Nightwatch.js Test' : function (browser) {
+  'Nightwatch.js Test': function (browser) {
     browser
       .url(browser.launch_url)
-      .waitForElementVisible('body', 1000)
+      .waitForElementVisible('#input', 1000)
+    browser
+      .pause(1000)
+      .clearValue('#input')
+      .expect.element('#input').text.to.equal('')
+    browser
       .setValue('#input', 'nightwatch')
-      .assert.containsText('#result', 'nightwatch')
-      .end();
+      .expect.element('#result').text.to.equal('nightwatch')
+    browser
+      .pause(1000)
+      .clearValue('#input')
+      .expect.element('#input').text.to.equal('')
+    browser
+      .setValue('#input', 'e2e')
+      .expect.element('#result').text.to.equal('e2e')
+    browser
+      .pause(1000)
+      .clearValue('#input')
+      .expect.element('#input').text.to.equal('')
+    browser
+      .setValue('#input', 'test')
+      .expect.element('#result').text.to.equal('test')
+    browser
+      .pause(1000)
+      .end()
   }
-};
+}
