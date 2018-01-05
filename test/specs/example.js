da40eab22c3835676042b484364f0e009d40a9f6
Sebastian Tschan <mail@blueimp.net>
2018-01-05 10:39:53 +0100

    Simplify sample spec.

---
+++ test/specs/example.js
@@ -0,0 +1,38 @@
+'use strict'
+
+function screenshotPath(browser) {
+  const path = require('path')
+  const dateTime = new Date()
+    .toISOString()
+    .split('.')[0]
+    .replace(/:/g, '-')
+  const fileName = `${browser.currentTest.module}-${dateTime}.png`
+  return path.resolve(
+    path.join(browser.globals.test_settings.screenshots.path || '', fileName)
+  )
+}
+
+module.exports = {
+  'sample test': function(browser) {
+    browser
+      .url(browser.launch_url)
+      .waitForElementVisible('#input', 1000)
+      .expect.element('#input')
+      .text.to.equal('')
+    browser
+      .pause(1000) // Prolong test for video demonstration
+      .setValue('#input', 'nightwatch')
+      .expect.element('#result')
+      .text.to.equal('nightwatch')
+    browser
+      .pause(1000) // Prolong test for video demonstration
+      .clearValue('#input')
+      .setValue('#input', 'test')
+      .expect.element('#result')
+      .text.to.equal('test')
+    browser
+      .pause(1000) // Prolong test for video demonstration
+      .saveScreenshot(screenshotPath(browser))
+      .end()
+  }
+}
